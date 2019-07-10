7e8b2ea7f19a098a7e4ba9224df1918c2a96bf7a
TornikeGomareli <gomareli@itstep.org>
2019-07-10 18:56:03 +0400

    added fps counter and fps display system, added todo tasks

--- Source/Rampage/ViewController.swift
+++ Source/Rampage/ViewController.swift
@@ -10,12 +10,14 @@
 import UIKit
 import Engine
 
+// viewController need refactoring
+
 private let joystickRadius: Double = 40
 private let maximumTimeStep: Double = 1 / 20
 private let worldTimeStep: Double = 1 / 120
 
 private func loadMap() -> Tilemap {
-    let jsonURL = Bundle.main.url(forResource: "Map", withExtension: "json")!
+    let jsonURL = Bundle.main.url(forResource: "Map", withExtension: "geojson")!
     let jsonData = try! Data(contentsOf: jsonURL)
     return try! JSONDecoder().decode(Tilemap.self, from: jsonData)
 }
@@ -25,11 +27,18 @@ class ViewController: UIViewController {
     private let panGesture = UIPanGestureRecognizer()
     private var world = World(map: loadMap())
     private var lastFrameTime = CACurrentMediaTime()
+    private var fpsLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpImageView()
         
+        fpsLabel.frame = CGRect(x: 25, y: 25, width: 200, height: 50)
+        fpsLabel.textColor = .white
+        
+        fpsLabel.text = "123"
+        
+        self.view.addSubview(fpsLabel)
         let displayLink = CADisplayLink(target: self, selector: #selector(update))
         displayLink.add(to: .main, forMode: .common)
         
@@ -52,8 +61,13 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    // need to refactor this method
     @objc func update(_ displayLink: CADisplayLink) {
         let timeStep = min(maximumTimeStep, displayLink.timestamp - lastFrameTime)
+        
+        calculateFramePerSecond(displayLink)
+        
         let input = Input(velocity: inputVector)
         let worldSteps = (timeStep / worldTimeStep).rounded(.up)
         for _ in 0 ..< Int(worldSteps) {
@@ -66,6 +80,15 @@ class ViewController: UIViewController {
         renderer.draw(world)
         
         imageView.image = UIImage(bitmap: renderer.bitmap)
+    }
+    
+    func calculateFramePerSecond(_ displayLink: CADisplayLink){
+        var retVal : Int = 60
+        
+         retVal = Int(round(1000 / displayLink.duration)/1000)
+        
+        
+        fpsLabel.text = "Current game Fps : \(retVal)"
     }
     
     func setUpImageView() {
