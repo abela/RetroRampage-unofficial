aaef5ff7cb00507a9270b3952db18583654e068e
TornikeGomareli <gomareli@itstep.org>
2019-07-11 13:10:00 +0400

    switched to first person, drawed angle, simple lighting

--- Source/Engine/Core/Renderer.swift
+++ Source/Engine/Core/Renderer.swift
@@ -16,18 +16,49 @@ public struct Renderer {
 
 public extension Renderer {
     mutating func draw(_ world: World) {
-        let scale = Double(bitmap.height) / world.size.y
+        let focalLength = 1.0
+        let viewWidth = 1.0
+        let viewPlane = world.player.direction.orthogonal * viewWidth
+        let viewCenter = world.player.position + world.player.direction * focalLength
+        let viewStart = viewCenter - viewPlane / 2
+        
+        // Cast rays
+        let columns = bitmap.width
+        let step = viewPlane / Double(columns)
+        var columnPosition = viewStart
+        for x in 0 ..< columns {
+            let rayDirection = columnPosition - world.player.position
+            let viewPlaneDistance = rayDirection.length
+            let ray = Ray(
+                origin: world.player.position,
+                direction: rayDirection / viewPlaneDistance
+            )
+            let end = world.map.hitTest(ray: ray)
+            let wallDistance = (end - ray.origin).length
+            
+            // Draw wall
+            let wallHeight = 1.0
+            
+            let distanceRatio = viewPlaneDistance / focalLength
+            let perpendicular = wallDistance / distanceRatio
+            let height = wallHeight * focalLength / perpendicular * Double(bitmap.height)
+            
+            let wallColor: Color
+            if end.x.rounded(.down) == end.x {
+                wallColor = .white
+            } else {
+                wallColor = .gray
+            }
 
-        drawMap(world, scale)
-        
-        drawPlayer(world, scale)
-        
-        
-        castRays(world, scale)
-        
-        
-        
-        drawViewPlane(world, scale)
+            
+            bitmap.drawLine(
+                from: Vector(x: Double(x), y: (Double(bitmap.height) - height) / 2),
+                to: Vector(x: Double(x), y: (Double(bitmap.height) + height) / 2),
+                color: wallColor
+            )
+            
+            columnPosition += step
+        }
         
     }
     
@@ -55,7 +86,7 @@ public extension Renderer {
     
         // must refactor this method
         let columns = 10
-        let viewWidth = 1.0
+        let viewWidth = Double(bitmap.width) / Double(bitmap.height)
         let focalLength = 1.0
         let viewPlane = world.player.direction.orthogonal * viewWidth
         let step = viewPlane / Double(columns)
