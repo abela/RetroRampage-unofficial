45bb4c6f5b7a136be576000a082c07ec26642d95
Tornike Gomareli <t.gomareli@spryflash.com>
2020-02-04 22:49:36 +0400

    added monsters

---
+++ Source/Engine/Core/Monster.swift
@@ -0,0 +1,17 @@
+//
+//  Monster.swift
+//  Engine
+//
+//  Created by Tornike Gomareli on 2/4/20.
+//  Copyright © 2020 Nick Lockwood. All rights reserved.
+//
+
+import Foundation
+
+public struct Monster {
+    public var position: Vector
+    
+    init(position: Vector) {
+        self.position = position
+    }
+}
